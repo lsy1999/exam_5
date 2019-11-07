da34192a1a78410eee3e17604e6bdb61bb189caa
liuyanan <1173502073@qq.com>
2019-11-07 08:09:56 +0800

    第一次提交

---
+++ src/test/java/com/bw/test/Demo.java
@@ -0,0 +1,16 @@
+package com.bw.test;
+
+public class Demo {
+
+	/**
+	 * 
+	 * @Title: main 
+	 * @Description:（二）编写main方法，输出helloWord。
+	 * @param args
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("helloWord");
+	}
+}
